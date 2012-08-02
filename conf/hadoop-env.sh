73057df61f6ce69d7ba6569171e8ae40f8012eaa
alushnikov <>
2012-08-02 15:49:56 -0700

    Fixes double-source problem in hadoop script
    Summary:
    This is the fix to the following problem.
    When starting hadoop instances via `hadoop-daemon.sh` script, it
    it at first sources `hadoop-env.sh` file. After some sequence
    of actions, it runs `hadoop`, which in turn sources
    `hadoop-env.sh` as well. As a result CLI arguments are
    doubled.
    So far it turned out that some of java arguments mustn't appear
    twice in the command line or JVM won't start (particularly I encountered it with -Xrunjdwp
    argument). This diff suggests a fix for the problem
    
    Test Plan:
    Try to run `./hadoop-daemon.sh` with different `hadoop-env.sh` files: one
    with -Xrunjdwp argument inside and one without.
    
    Reviewers: tomasz, hkuang, sdong, avf, mbautin
    
    Reviewed By: tomasz

--- conf/hadoop-env.sh
+++ conf/hadoop-env.sh
@@ -1,68 +1,74 @@
-# Set Hadoop-specific environment variables here.
+# This if statement insures that this file will be sources only once
+if [[ "$IS_HADOOP_ENV_ALREADY_SOURCED" != "true" ]]; then
+  export IS_HADOOP_ENV_ALREADY_SOURCED="true";
 
-# The only required environment variable is JAVA_HOME.  All others are
-# optional.  When running a distributed configuration it is best to
-# set JAVA_HOME in this file, so that it is correctly defined on
-# remote nodes.
+  # Set Hadoop-specific environment variables here.
 
-# The java implementation to use.  Required.
-# export JAVA_HOME=/usr/lib/j2sdk1.5-sun
+  # The only required environment variable is JAVA_HOME.  All others are
+  # optional.  When running a distributed configuration it is best to
+  # set JAVA_HOME in this file, so that it is correctly defined on
+  # remote nodes.
 
-# Extra Java CLASSPATH elements.  Optional.
-# export HADOOP_CLASSPATH=
+  # The java implementation to use.  Required.
+  # export JAVA_HOME=/usr/lib/j2sdk1.5-sun
 
-# The maximum amount of heap to use, in MB. Default is 1000.
-# export HADOOP_HEAPSIZE=2000
+  # Extra Java CLASSPATH elements.  Optional.
+  # export HADOOP_CLASSPATH=
 
-# Extra Java runtime options.  Empty by default.
-# export HADOOP_OPTS=-server
+  # The maximum amount of heap to use, in MB. Default is 1000.
+  # export HADOOP_HEAPSIZE=2000
 
-# Command specific options appended to HADOOP_OPTS when specified
-export HADOOP_NAMENODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_NAMENODE_OPTS"
-export HADOOP_SECONDARYNAMENODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_SECONDARYNAMENODE_OPTS"
-export HADOOP_DATANODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_DATANODE_OPTS"
-export HADOOP_BALANCER_OPTS="-Dcom.sun.management.jmxremote $HADOOP_BALANCER_OPTS"
-export HADOOP_JOBTRACKER_OPTS="-Dcom.sun.management.jmxremote $HADOOP_JOBTRACKER_OPTS"
-export HADOOP_RAIDNODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_RAIDNODE_OPTS"
-export HADOOP_NAMENODE_OPTS="-Dcom.sun.management.jmxremote.port=8998 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+  # Extra Java runtime options.  Empty by default.
+  # export HADOOP_OPTS=-server
 
-# The only user who can start hadoop daemons. 
-# If this is not set, any user can start hadoop daemons.  
-export HADOOP_USERNAME="hadoop"
+  # Command specific options appended to HADOOP_OPTS when specified
+  export HADOOP_NAMENODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_NAMENODE_OPTS"
+  export HADOOP_SECONDARYNAMENODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_SECONDARYNAMENODE_OPTS"
+  export HADOOP_DATANODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_DATANODE_OPTS"
+  export HADOOP_BALANCER_OPTS="-Dcom.sun.management.jmxremote $HADOOP_BALANCER_OPTS"
+  export HADOOP_JOBTRACKER_OPTS="-Dcom.sun.management.jmxremote $HADOOP_JOBTRACKER_OPTS"
+  export HADOOP_RAIDNODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_RAIDNODE_OPTS"
+  export HADOOP_NAMENODE_OPTS="-Dcom.sun.management.jmxremote.port=8998 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 
-# Java Runtime garbage collection options to pass to all Hadoop
-# servers (Namenode, Jobtracker, Datanode, Tasktracker). This must end
-# with a colon ; to which the dynamically generated gc log filename will
-# be appended to. The below defaults work for the Sun JVM, for example
-# in IBM GC, use '-Xverbosegclog:'.
-#export HADOOP_GC_LOG_OPTS="-XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:"
+  # The only user who can start hadoop daemons.
+  # If this is not set, any user can start hadoop daemons.
+  export HADOOP_USERNAME="hadoop"
 
-# export HADOOP_TASKTRACKER_OPTS=
-# The following applies to multiple commands (fs, dfs, fsck, distcp etc)
-# export HADOOP_CLIENT_OPTS
+  # Java Runtime garbage collection options to pass to all Hadoop
+  # servers (Namenode, Jobtracker, Datanode, Tasktracker). This must end
+  # with a colon ; to which the dynamically generated gc log filename will
+  # be appended to. The below defaults work for the Sun JVM, for example
+  # in IBM GC, use '-Xverbosegclog:'.
+  #export HADOOP_GC_LOG_OPTS="-XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:"
 
-# Extra ssh options.  Empty by default.
-# export HADOOP_SSH_OPTS="-o ConnectTimeout=1 -o SendEnv=HADOOP_CONF_DIR"
+  # export HADOOP_TASKTRACKER_OPTS=
+  # The following applies to multiple commands (fs, dfs, fsck, distcp etc)
+  # export HADOOP_CLIENT_OPTS
 
-# Where log files are stored.  $HADOOP_HOME/logs by default.
-# export HADOOP_LOG_DIR=${HADOOP_HOME}/logs
+  # Extra ssh options.  Empty by default.
+  # export HADOOP_SSH_OPTS="-o ConnectTimeout=1 -o SendEnv=HADOOP_CONF_DIR"
 
-# File naming remote slave hosts.  $HADOOP_HOME/conf/slaves by default.
-# export HADOOP_SLAVES=${HADOOP_HOME}/conf/slaves
+  # Where log files are stored.  $HADOOP_HOME/logs by default.
+  # export HADOOP_LOG_DIR=${HADOOP_HOME}/logs
 
-# host:path where hadoop code should be rsync'd from.  Unset by default.
-# export HADOOP_MASTER=master:/home/$USER/src/hadoop
+  # File naming remote slave hosts.  $HADOOP_HOME/conf/slaves by default.
+  # export HADOOP_SLAVES=${HADOOP_HOME}/conf/slaves
 
-# Seconds to sleep between slave commands.  Unset by default.  This
-# can be useful in large clusters, where, e.g., slave rsyncs can
-# otherwise arrive faster than the master can service them.
-# export HADOOP_SLAVE_SLEEP=0.1
+  # host:path where hadoop code should be rsync'd from.  Unset by default.
+  # export HADOOP_MASTER=master:/home/$USER/src/hadoop
 
-# The directory where pid files are stored. /tmp by default.
-# export HADOOP_PID_DIR=/var/hadoop/pids
+  # Seconds to sleep between slave commands.  Unset by default.  This
+  # can be useful in large clusters, where, e.g., slave rsyncs can
+  # otherwise arrive faster than the master can service them.
+  # export HADOOP_SLAVE_SLEEP=0.1
 
-# A string representing this instance of hadoop. $USER by default.
-# export HADOOP_IDENT_STRING=$USER
+  # The directory where pid files are stored. /tmp by default.
+  # export HADOOP_PID_DIR=/var/hadoop/pids
 
-# The scheduling priority for daemon processes.  See 'man nice'.
-# export HADOOP_NICENESS=10
+  # A string representing this instance of hadoop. $USER by default.
+  # export HADOOP_IDENT_STRING=$USER
+
+  # The scheduling priority for daemon processes.  See 'man nice'.
+  # export HADOOP_NICENESS=10
+
+fi
