169854de93a4b0ae86f56d98981f4f31dc196348
tomasz <>
2012-09-05 10:32:44 -0700

    [L38-6] Introduce new layout storage inspector
    Summary:
    This patch introduces the inpector for L38 storage directories, it's
    a
    counterpart for FSImagePretransactionalStorageInspector. This
    inspector
    will be used once we bump up the layout number.
    
    Test Plan: added test case for the inspector
    
    Reviewers: hkuang, pritam, avf
    
    Reviewed By: avf
    
    Differential Revision:

--- src/test/org/apache/hadoop/hdfs/server/namenode/FSImageTestUtil.java
+++ src/test/org/apache/hadoop/hdfs/server/namenode/FSImageTestUtil.java
@@ -17,12 +17,21 @@
  */
 package org.apache.hadoop.hdfs.server.namenode;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hdfs.server.common.Storage.StorageDirType;
+import org.apache.hadoop.hdfs.server.common.Storage.StorageDirectory;
+import org.apache.hadoop.hdfs.server.namenode.NNStorage.NameNodeDirType;
 import org.apache.hadoop.hdfs.util.InjectionEvent;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 
 /**
@@ -31,6 +40,61 @@ import org.apache.hadoop.hdfs.util.InjectionEvent;
 public abstract class FSImageTestUtil {
   
   final static Log LOG = LogFactory.getLog(FSImageTestUtil.class);
+  
+  public static StorageDirectory mockStorageDirectory(
+      File currentDir, NameNodeDirType type) {
+    // Mock the StorageDirectory interface to just point to this file
+    StorageDirectory sd = Mockito.mock(StorageDirectory.class);
+    Mockito.doReturn(type)
+      .when(sd).getStorageDirType();
+    Mockito.doReturn(currentDir).when(sd).getCurrentDir();
+    Mockito.doReturn(currentDir).when(sd).getRoot();
+    Mockito.doReturn(mockFile(true)).when(sd).getVersionFile();
+    Mockito.doReturn(mockFile(false)).when(sd).getPreviousDir();
+    return sd;
+  }
+  
+  /**
+   * Make a mock storage directory that returns some set of file contents.
+   * @param type type of storage dir
+   * @param previousExists should we mock that the previous/ dir exists?
+   * @param fileNames the names of files contained in current/
+   */
+  static StorageDirectory mockStorageDirectory(
+      StorageDirType type,
+      boolean previousExists,
+      String...  fileNames) {
+    StorageDirectory sd = mock(StorageDirectory.class);
+    
+    doReturn(type).when(sd).getStorageDirType();
+  
+    // Version file should always exist
+    doReturn(mockFile(true)).when(sd).getVersionFile();
+    doReturn(mockFile(true)).when(sd).getRoot();
+
+    // Previous dir optionally exists
+    doReturn(mockFile(previousExists))
+      .when(sd).getPreviousDir();   
+  
+    // Return a mock 'current' directory which has the given paths
+    File[] files = new File[fileNames.length];
+    for (int i = 0; i < fileNames.length; i++) {
+      files[i] = new File(fileNames[i]);
+    }
+    
+    File mockDir = Mockito.spy(new File("/dir/current"));
+    doReturn(files).when(mockDir).listFiles();
+    doReturn(mockDir).when(sd).getCurrentDir();
+    
+
+    return sd;
+  }
+  
+  static File mockFile(boolean exists) {
+    File mockFile = mock(File.class);
+    doReturn(exists).when(mockFile).exists();
+    return mockFile;
+  }
   
   public static class CheckpointTrigger {
     volatile boolean triggerCheckpoint = false;
