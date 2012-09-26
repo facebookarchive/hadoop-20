169183fc4b6b86c10dc9c560811a20641a3d7ba5
pkhemani <>
2012-09-26 11:45:12 -0700

    Allow hadoop daemons to run as root
    Summary: The fsshellservice should run as root. This lets it easily read and write local files. Also the interaction with other fb services is better because they run as root as well.
    
    Test Plan: HADOOP_USERNAME=root bin/hadoop start fsshellservice
    
    Reviewers: sdong, pknowles, hkuang, hsouza
    
    Reviewed By: hsouza

--- conf/hadoop-env.sh
+++ conf/hadoop-env.sh
@@ -32,7 +32,8 @@ if [[ "$IS_HADOOP_ENV_ALREADY_SOURCED" != "true" ]]; then
 
   # The only user who can start hadoop daemons.
   # If this is not set, any user can start hadoop daemons.
-  export HADOOP_USERNAME="hadoop"
+  HADOOP_USERNAME=${HADOOP_USERNAME:-hadoop}
+  export HADOOP_USERNAME
 
   # Java Runtime garbage collection options to pass to all Hadoop
   # servers (Namenode, Jobtracker, Datanode, Tasktracker). This must end
