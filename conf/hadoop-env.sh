908f1bf7bfbb8c1270ca2bb35885d7d57e446eca
tomasz <>
2012-09-27 14:23:58 -0700

    Fix bug in FSEditLog
    Summary:
    Fixed the logic. Since we have the finally block, we need to ensure that
    we were actually syncing if we want to set isSyncRunning, and notifyAll.
    Because now, the finally block is executed even if we exit earlier.
    
    @Alex please run the test cases before committing code.
    
    Test Plan: ant test TestEditLogRace, TestEditLog
    
    Reviewers: avf, hkuang
    
    Reviewed By: avf
    
    Blame Revision: D580320

--- conf/hadoop-env.sh
+++ conf/hadoop-env.sh
@@ -32,8 +32,7 @@ if [[ "$IS_HADOOP_ENV_ALREADY_SOURCED" != "true" ]]; then
 
   # The only user who can start hadoop daemons.
   # If this is not set, any user can start hadoop daemons.
-  HADOOP_USERNAME=${HADOOP_USERNAME:-hadoop}
-  export HADOOP_USERNAME
+  export HADOOP_USERNAME="hadoop"
 
   # Java Runtime garbage collection options to pass to all Hadoop
   # servers (Namenode, Jobtracker, Datanode, Tasktracker). This must end
